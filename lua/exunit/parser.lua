b6b8dcff52d09e8bb1fb054ca044e8f4c814fa14
Adam Rutkowski <hq@mtod.org>
2025-12-15 10:56:36 +0100

    Don't open loclist if already opened

--- lua/exunit/parser.lua
+++ lua/exunit/parser.lua
@@ -49,23 +49,34 @@ function M.populate_loclist(locations, config)
 	vim.fn.setloclist(0, {}, "a", { title = "ExUnit Test Failures" })
 
 	if config and config.location_list_mode and config.location_list_mode ~= "manual" then
-		local current_bufname = vim.api.nvim_buf_get_name(0)
-		local current_in_loclist = false
-		
-		for _, item in ipairs(items) do
-			local item_path = vim.fn.getcwd() .. "/" .. item.filename
-			if current_bufname == item_path then
-				current_in_loclist = true
+		local loclist_already_open = false
+		for _, win in ipairs(vim.api.nvim_list_wins()) do
+			local buf = vim.api.nvim_win_get_buf(win)
+			if vim.bo[buf].filetype == "qf" and vim.fn.getloclist(vim.fn.win_getid(win)) ~= {} then
+				loclist_already_open = true
 				break
 			end
 		end
 		
-		if not current_in_loclist then
-			if config.location_list_mode == "focus" then
-				vim.cmd("lopen")
-			elseif config.location_list_mode == "open_no_focus" then
-				vim.cmd("lopen")
-				vim.cmd("wincmd p")
+		if not loclist_already_open then
+			local current_bufname = vim.api.nvim_buf_get_name(0)
+			local current_in_loclist = false
+			
+			for _, item in ipairs(items) do
+				local item_path = vim.fn.getcwd() .. "/" .. item.filename
+				if current_bufname == item_path then
+					current_in_loclist = true
+					break
+				end
+			end
+			
+			if not current_in_loclist then
+				if config.location_list_mode == "focus" then
+					vim.cmd("lopen")
+				elseif config.location_list_mode == "open_no_focus" then
+					vim.cmd("lopen")
+					vim.cmd("wincmd p")
+				end
 			end
 		end
 	end
